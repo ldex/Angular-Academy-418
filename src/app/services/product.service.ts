f7719dd276b199dfb36d9093e260e1d85cb55f72
ldex <laurent.duveau+git@gmail.com>
2024-11-25 16:28:24 -0500

    Http Request

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { inject } from '@angular/core';
+import { delay, Observable, tap } from 'rxjs';
+import { Product } from '../models/product.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl = 'https://671d383409103098807c943e.mockapi.io/api/products/';
+  private http = inject(HttpClient)
+
+  products$: Observable<Product[]>
+
+  constructor() {
+    this.initProducts()
+  }
+
+  initProducts() {
+    this.products$ = this
+                      .http
+                      .get<Product[]>(this.baseUrl)
+                      .pipe(
+                          delay(1500), // For the demo!!!
+                          tap(console.table)
+                      )
+  }
+
 }
